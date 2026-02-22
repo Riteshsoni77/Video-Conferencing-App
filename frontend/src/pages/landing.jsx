ecaba01764415a85e38245c0668561666498d5fe
Riteshsoni77 <riteshsoni1138@gmail.com>
2026-02-22 19:07:27 +0530

    setup  the frontend and add a landingpage

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,42 @@
+import react from 'react'
+import"../App.css"
+import { Link } from 'react-router-dom'
+
+
+
+export default function LandingPage() {
+    return (
+        <div className='landingPageContainer'>
+          <nav>
+            <div className='navheader'>
+                <h2>Video Conferencing App </h2>
+                 </div>
+            <div className='navlist'>
+                <p> Join as Guest</p>
+                <p> Register</p>
+               <div role='button'>
+                <p>Login</p>
+                 </div>
+            </div>
+          </nav>
+
+         <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Video Call</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                    <img src="src/assets/mobile.png" alt="" />
+
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
